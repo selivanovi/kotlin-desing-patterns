11694e8f4ad02eee880ff405e8c6c3aa09533682
Illia Selivanov <ilia.selivanon@nitrix.io>
2023-03-01 14:03:58 +0300

    create Factory Method

--- src/test/kotlin/AbstractFactoryTest.kt
+++ src/test/kotlin/AbstractFactoryTest.kt
@@ -1,13 +1,40 @@
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
+interface Plant
+
+class OrangePlant : Plant
+
+class ApplePlant : Plant
+
+abstract class PlantFactory {
+
+    abstract fun makePlant(): Plant
+
+    companion object {
+        inline fun <reified T : Plant> createFactory(): PlantFactory =
+            when (T::class) {
+                OrangePlant::class -> OrangeFactory()
+                ApplePlant::class -> AppleFactory()
+                else -> throw IllegalArgumentException()
+            }
+    }
+}
+
+class AppleFactory : PlantFactory() {
+    override fun makePlant(): Plant = ApplePlant()
+}
+
+class OrangeFactory : PlantFactory() {
+    override fun makePlant(): Plant = OrangePlant()
+}
+
 class AbstractFactoryTest {
 
     @Test
     fun `Abstract Factory`() {
         val plantFactory = PlantFactory.createFactory<OrangePlant>()
         val plant = plantFactory.makePlant()
-        println("Created plant: $plant")
 
         assertEquals(OrangePlant::class, plant::class)
     }
